d8ca82ba1e41050bb639b7c2a15c014cd800b946
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2023-04-07 01:40:20 +0100

    simulator done

--- luisteofilo-ai-solve-games-b96806e60704/src/games/texasholdem/simulator.py
+++ luisteofilo-ai-solve-games-b96806e60704/src/games/texasholdem/simulator.py
@@ -13,23 +13,42 @@ class TexasSimulator(GameSimulator):
         """
         deck of cards
         """
-        self.__deck = [TexasCard.Jack, TexasCard.Queen, TexasCard.King]
+        deck = TexasCard()
+        self.__deck = deck.get_cards()
+        # self.__deck = [TexasCard.Jack, TexasCard.Queen, TexasCard.King]
 
     def init_game(self):
         # shuffle the deck
         shuffle(self.__deck)
 
-        # assign a card to each player
+        # assign two cards to each player
         positions = self.get_player_positions()
-        for pos in range(0, len(positions)):
-            positions[pos].set_current_card(self.__deck[pos])
+        for player in positions:
+            cards = self.__deck[:2]
+            player.set_current_cards(cards)
+            self.__deck = self.__deck[2:]
+
+        # assign three community cards
+        for i in range(3):
+            self.state.add_community_card(self.__deck.pop())
+
         return TexasState()
 
+        # assign a card to each player
+        # positions = self.get_player_positions()
+        # for pos in range(0, len(positions)):
+        #     positions[pos].set_current_card(self.__deck[pos])
+
     def before_end_game(self, state: TexasState):
-        # if we reached the show down, we are going to reveal the cards to all players
-        if state.is_showdown():
-            for pos in range(0, self.num_players()):
+        # reveal all the cards when the game is over, one player folds, or last round of betting
+        if state.is_game_over() or state.get_active_players() == 1 or state.get_current_betting_round() == 4:
+            for pos in range(self.num_players()):
                 state.draw_card(pos, self.__deck[pos])
+
+        # if we reached the show down, we are going to reveal the cards to all players
+        # if state.is_showdown():
+        #     for pos in range(0, self.num_players()):
+        #         state.draw_card(pos, self.__deck[pos])
 
     def end_game(self, state: TexasState):
         # ignored for this simulator
