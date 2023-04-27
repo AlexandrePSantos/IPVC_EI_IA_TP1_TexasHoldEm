e79434939fbde3c7acf860644b2d23354ce7d9cf
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2023-04-27 16:29:59 +0100

    cfr
    inicio

--- luisteofilo-ai-solve-games-b96806e60704/src/games/texasholdem/players/cfr_easy.py
+++ luisteofilo-ai-solve-games-b96806e60704/src/games/texasholdem/players/cfr_easy.py
@@ -2,15 +2,26 @@ from games.texasholdem.action import TexasAction
 from games.texasholdem.player import TexasPlayer
 from games.texasholdem.state import TexasState
 from games.state import State
+from random import random
 
 
 class CFREasyTexasPlayer(TexasPlayer):
 
     def __init__(self, name):
         super().__init__(name)
+        self.__trainer = TexasTrainer()
+        self.__trainer.train(100000)
 
     def get_action(self, state: TexasState):
-        return TexasAction.BET
+        info_set = f"{self.get_current_card()}"
+        for action in state.get_sequence():
+            if action == TexasAction.PASS:
+                info_set += 'p'
+            else:
+                info_set += 'b'
+        prob = self.__trainer.get_avg_strategy(info_set)[0]
+
+        return TexasAction.PASS if random() <= prob else TexasAction.BET
 
     def event_action(self, pos: int, action, new_state: State):
         # ignore
--- luisteofilo-ai-solve-games-b96806e60704/src/games/texasholdem/players/cfr_hard.py
+++ luisteofilo-ai-solve-games-b96806e60704/src/games/texasholdem/players/cfr_hard.py
@@ -2,15 +2,29 @@ from games.texasholdem.action import TexasAction
 from games.texasholdem.player import TexasPlayer
 from games.texasholdem.state import TexasState
 from games.state import State
+from random import random
 
 
 class CFRHardTexasPlayer(TexasPlayer):
 
     def __init__(self, name):
         super().__init__(name)
+        self.__trainer = TexasTrainer()
+        self.__trainer.train(100000)
 
     def get_action(self, state: TexasState):
-        return TexasAction.BET
+        info_set = f"{self.get_current_card()}"
+        for action in state.get_sequence():
+            if action == TexasAction.PASS:
+                info_set += 'p'
+            elif action == TexasAction.CALL:
+                info_set += 'c'
+            elif action == TexasAction.RAISE:
+                info_set += 'r'
+        prob = self.__trainer.get_avg_strategy(info_set)[0]
+
+        # Verificar esta parte para adicionar o RAISE
+        return TexasAction.PASS if random() <= prob else TexasAction.CALL
 
     def event_action(self, pos: int, action, new_state: State):
         # ignore
